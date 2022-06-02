247ffde77cb8571f16bc5481ddd007c1cf4680bf
Emanuel Riaño Mora <92994002+KuroganeRM@users.noreply.github.com>
2022-06-01 20:51:54 -0500

    Add files via upload

---
+++ SnackConference/main.js
@@ -0,0 +1,16 @@
+/*
+
+const singupForm = document.querySelector('#singup-form');
+
+singupForm.addEventListener('submit',(e) => {
+    e.preventDefault();
+
+    const email = document.querySelector('#singup-email').value;
+    const password = document.querySelector('#singup-password').value;
+
+    auth.createUserWithEmailAndPassword(email, password)
+        .then(userCredential => {
+            console.log('sing up')
+        })
+})
+*/
